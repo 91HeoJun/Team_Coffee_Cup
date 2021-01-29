d13bb33ba3a93c07ecfcbbb7f3af0b6600a4c11c
[Jun Heo] <[c_atuniel@naver.com]>
2021-01-29 15:52:06 +0900

    Board Mapper SQL Query Update

--- TeamProjectCoffee/TeamProjectCoffee.sql
+++ TeamProjectCoffee/TeamProjectCoffee.sql
@@ -3,8 +3,6 @@ CREATE TABLE UserTBL (
 	userid		VARCHAR2(15), 
 	password	VARCHAR2(20)	NOT NULL, 
 	name		NVARCHAR2(10)	NOT NULL, 
-	gender		VARCHAR2(2)		NOT NULL, 
-	birth		DATE			NOT NULL, 
 	addr		VARCHAR2(50)	NOT NULL, 
 	email		VARCHAR2(50)	NOT NULL, 
 	tel			VARCHAR2(20)	NOT NULL, 
