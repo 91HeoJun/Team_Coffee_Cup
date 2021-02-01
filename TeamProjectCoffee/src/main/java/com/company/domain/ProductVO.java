eb8de5ce2f988a11fc12009ce8e7902d93c1cb46
yejin <goyejin25@gmail.com>
2021-02-01 15:40:29 +0900

    상품 기초작업 업데이트

--- TeamProjectCoffee/src/main/java/com/company/domain/ProductVO.java
+++ TeamProjectCoffee/src/main/java/com/company/domain/ProductVO.java
@@ -1,5 +1,25 @@
 package com.company.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+//상품VO
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductVO {
-
+	private int pCode;  //상품코드
+	private String pcategory;  //상품종류
+	private String product;  //상품이름
+	private String pContent; //상품 상세내용
+	private String pDate;  //상품 제조(로스팅)일자
+	private int price;  //상품가격
+	private int pAmount;  //상품 재고수량
+	private boolean image;  //상품이미지
+	private String origin;  //상품원산지
+	
 }
--- TeamProjectCoffee/src/main/java/com/company/mapper/ProductMapper.java
+++ TeamProjectCoffee/src/main/java/com/company/mapper/ProductMapper.java
@@ -1,8 +1,14 @@
 package com.company.mapper;
 
+import java.util.List;
+
 import com.company.domain.ProductVO;
 
 public interface ProductMapper {
 	
-	public int insert(ProductVO vo); 
+	public int insert(ProductVO vo);
+	public int update(ProductVO vo);
+	public int delete(int code);
+	public ProductVO select(int code);
+	public List<ProductVO> selectAll();	
 }
