4797de23b636bebd66b1f696a83ad074eec27561
[Jun Heo] <[c_atuniel@naver.com]>
2021-02-02 22:07:26 +0900

    페이지나누기

---
+++ TeamProjectCoffee/src/main/java/com/company/domain/Criteria.java
@@ -0,0 +1,34 @@
+package com.company.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+
+	// 페이지 나누기
+	private int pageNum;	// 페이지 번호
+	private int amount;		// 페이지내 게시물 수
+	
+	// 페이지당 당 게시물수
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+	
+	public Criteria() {
+		this(1, 10);
+	}
+	
+	// 검색기
+	private String type;	// 검색 카테고리
+	private String keyword;	// 검색어
+	
+	// 검색타입 배열처리
+	public String[] getTypeArr() {
+		return type==null?new String[] {}:type.split("");
+	}
+}
